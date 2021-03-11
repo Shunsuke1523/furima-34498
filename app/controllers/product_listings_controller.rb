7ab2be8c4ebd02f18c58fd37dfb162083272e685
Shunsuke1523 <ki840y2er5@gmail.com>
2021-03-11 18:39:57 +0900

    商品情報編集機能

--- app/controllers/product_listings_controller.rb
+++ app/controllers/product_listings_controller.rb
@@ -1,5 +1,6 @@
 class ProductListingsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: :edit
 
   def index
     @product_listings = ProductListing.all.order(created_at: 'DESC')
@@ -22,10 +23,27 @@ class ProductListingsController < ApplicationController
     @product_listing = ProductListing.find(params[:id])
   end
 
+  def edit
+    @product_listing = ProductListing.find(params[:id])
+  end
+
+  def update
+    @product_listing = ProductListing.find(params[:id])
+    if @product_listing.update(product_listing_params)
+      redirect_to product_listing_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_listing_params
     params.require(:product_listing).permit(:image, :product_name, :product_description, :category_id, :product_status_id,
                                             :delivery_fee_burden_id, :delivery_area_id, :delivery_days_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless current_user.id == ProductListing.find(params[:id]).user.id
   end
 end
